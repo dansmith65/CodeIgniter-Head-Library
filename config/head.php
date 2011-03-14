69ab3c0fc717f2e68b2d13f74db81cb7f699dc86
Dan Smith <dansmith65@gmail.com>
2011-03-14 12:29:28 -0700

    v2.0

--- config/head.php
+++ config/head.php
@@ -1,48 +1,73 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
- // --------------------------------------------------------------------------
- // Head Library Configs
- // --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// Head Library Configs
+// --------------------------------------------------------------------------
 
- /**
- * Default configs
- *
- * These configs can override class variables for the head class. You can find
- * These variables listed and commented in the head class file.
- */
-  
- $config['meta_author'] = "Adam Fairholm";
- 
- $config['meta_description'] = "A test page.";
- 
- /**
- * Default elements
- *
- * Need some files loaded for every page? Put them here. They will be loaded first.
- */
- 
- $config['defaults'] = array('master.css', 'tinymce/tinymce.js');
- 
- $config['defaults']['packages'] = array('coda_slider');
+/*
+|--------------------------------------------------------------------------
+| Default configs
+|--------------------------------------------------------------------------
+|
+| These configs can override class variables for the head class. You can find
+| These variables listed and commented in the head class file.
+|
+*/
 
- /**
- * Packages
- *
- * Packages work like containers for groups of files needed. For example, the coda slider
- * Needs several Javascipt files, and a css file.
- *
- * Duplicate files in packages will not loaded twice. 
- */
- 
- $config['packages']['coda_slider'] = array(
-	'coda-slider.css',
-	'jquery.js',
+$config['show_errors'] = TRUE;
+$config['meta_author'] = "Adam Fairholm";
+$config['meta_description'] = "A test page.";
+$config['site_title'] = 'Accommodating Ideas';
+$config['jquery_file'] = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+
+
+/*
+|--------------------------------------------------------------------------
+| Default elements
+|--------------------------------------------------------------------------
+|
+| Need some files loaded for every page? Put them here. They will be loaded first.
+|
+*/
+
+$config['defaults']['css'][] = 'styles.css';
+$config['defaults']['js'][] = array('https://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE7.js', 'IE 7');
+
+$config['defaults']['packages'] = array('coda_slider', 'accordian');
+
+
+/*
+|--------------------------------------------------------------------------
+| Packages
+|--------------------------------------------------------------------------
+|
+| Packages work like containers for groups of files needed. For example, the coda slider
+| needs several Javascipt files, and a css file.
+| 
+| Packages can contain references to other packages. Each referenced package will be
+| recursively loaded when the main package is loaded.
+|
+*/
+
+// JQUERY
+$config['packages']['jquery']['js'][] = $config['jquery_file'];
+
+// CODA_SLIDER
+$config['packages']['coda_slider']['packages'][] = 'jquery';
+$config['packages']['coda_slider']['css'][] = 'coda-slider.css';
+$config['packages']['coda_slider']['js'] = array(
 	'jquery.scrollTo.js',
 	'jquery.localscroll.js',
 	'jquery.serialScroll.js',
-	'coda-slider.js');
-	
- $config['packages']['accordian']		= array('jquery.js', 'accordian.js', 'accordian.css');
+	'coda-slider.js'
+);
+
+// ACCORDIAN
+$config['packages']['accordian']['packages'][] = 'jquery';
+$config['packages']['accordian']['css'] ='accordian.css';
+$config['packages']['accordian']['js'] = 'accordian.js';
+
+
 
 /* End of file head.php */
 /* Location: ./application/config/head.php */
